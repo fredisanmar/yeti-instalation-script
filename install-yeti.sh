828a67e906aaf7d19330989e6d53dbf0893ef8c2
fredisanmar <40606800+fredisanmar@users.noreply.github.com>
2018-07-26 11:02:40 +0200

    Update install-yeti.sh

--- install-yeti.sh
+++ install-yeti.sh
@@ -12,7 +12,7 @@ apt-get install -y apt-transport-https
 echo "====================instalando yarn=================================="
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg |  apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" |  tee /etc/apt/sources.list.d/yarn.list
-apt-get update &&  apt-get install -y yarn 
+apt-get update && apt purge cmdtest && apt-get install -y yarn 
 echo "====================yarn instalado==================================="
 echo "====================clonando yeti===================================="
 cd /
